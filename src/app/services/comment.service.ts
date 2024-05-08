da528122169b10acc9d22274801429738568cbff
Nika <white4ik1995@gmail.com>
2024-05-08 23:45:40 +0400

    add user & comment interfacces

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { enviroment } from '../enviroment';
+import { Comment } from '../interfaces/comment.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
-  http = inject(HttpClient)
+  http = inject(HttpClient);
 
-  getComments(parentId: string = ''){
-    let url = `${enviroment.apiBaseUrl}/comments`
-    if(parentId){
-      url+= `parentId=${parentId}`
+  getComments(parentId: string = '') {
+    let url = `${enviroment.apiBaseUrl}/comments`;
+    if (parentId) {
+      url += `?parentId=${parentId}`;
     }
-    this.http.get(url)
+    return this.http.get<Comment[]>(url);
   }
 
-  constructor() { }
+  constructor() {}
 }
