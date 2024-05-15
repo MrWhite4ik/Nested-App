638fdc1f1f63ff61303b882930ec6ffab99d1323
Nika <white4ik1995@gmail.com>
2024-05-15 19:19:50 +0400

    add logic to create user

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { enviroment } from '../enviroment';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  http = inject(HttpClient);
+  localStorageKey = 'nestedapp_user';
+  createUser(name: string) {
+    return this.http.post<User>(`${enviroment.apiBaseUrl}/users`, {
+      name,
+    });
+  }
+  saveUserToStorage(user: User) {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(user));
+  }
+  getUserFromStorage() {
+    const user = localStorage.getItem(this.localStorageKey);
+    return user ? (JSON.parse(user) as User) : null;
+  }
+}
